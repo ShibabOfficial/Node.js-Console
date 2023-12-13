2c9e46a30292d8c7eba068c0a2d4f47c889c3122
ShibabOfficial <88441414+ShibabOfficial@users.noreply.github.com>
2023-12-13 21:56:21 +0100

    small fixes

--- src/modules/console/commands/console/exit.js
+++ src/modules/console/commands/console/exit.js
@@ -6,9 +6,6 @@ module.exports = {
     desc: "exits the whole console",
     async func(info, args) {
         LOG("Shutting down...");
-        if (info.bot.running)
-            shutdown(info.bot.threadID);
-
         console.log("\n Goodbye! \n");
         process.exit(0);
     } 
